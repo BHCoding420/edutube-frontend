8c7ea92a1a95de4d76d838ce09d52185b08d1e0c
BHCoding420 <81071869+BHCoding420@users.noreply.github.com>
2024-06-16 15:33:43 +0100

    Update Homepage.tsx

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -18,7 +18,7 @@ function App() {
     useEffect(() => {
       console.log(process.env);
       const data = axios
-        .get(`https://edutubeserver-production.up.railway.app/tutorials`)
+        .get(`https://edutube-server.onrender.com/tutorials`)
         .then((response) => {
           console.log(response);
           setvideos(response.data.tutorials);
